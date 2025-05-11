25885d4039d0e2ddf5b184bfa9d8d2fe258d79bb
Luca Bozzetti <luca.bozzetti8@gmail.com>
2025-05-11 02:06:51 +0200

    all python DONE!

--- server/image_engine/document_mapping.py
+++ server/image_engine/document_mapping.py
@@ -5,7 +5,7 @@ def mapping(cl):
     return {
         "blutbild": (Blutbild, blutbild_prompt, cl.save_bloodtest),
         "impfpass": (Impfpass, impfpass_prompt, cl.save_impfung),
-        "befund": (Befund, befund_prompt),
+        "befund": (Befund, befund_prompt, cl.save_report),
         "medikation": (Medikation, medikation_prompt, cl.save_medication),
         "other": (Befund, other_prompt)
     }
